ea43dc2abb87c36ee42716f5609f1b5d8810fe30
FreakTheMighty <jvanderdoes@gmail.com>
2012-12-02 14:20:45 -0800

    Dart Web Component Test Project

---
+++ web/WebComponentTest2.dart
@@ -0,0 +1,17 @@
+import 'dart:html';
+
+void main() {
+  var dataValue = "Jesse";
+  query("#text")
+    ..text = "Click me!"
+    ..on.click.add(reverseText);
+}
+
+void reverseText(Event event) {
+  var text = query("#text").text;
+  var buffer = new StringBuffer();
+  for (int i = text.length - 1; i >= 0; i--) {
+    buffer.add(text[i]);
+  }
+  query("#text").text = buffer.toString();
+}
